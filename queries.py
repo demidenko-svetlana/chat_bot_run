6cd3ac4b8e18b1c99a8ae315f8a242b84add2489
Kate Sm <yukaicat@gmail.com>
2020-04-22 15:31:19 +0300

    доработка запросов из БД в цельные сообщения

--- queries.py
+++ queries.py
@@ -1,25 +1,34 @@
+from sqlalchemy import text
 from database import Event, session
 
+def rus_event(country):
+    rus_location = session.query(Event) \
+        .filter(Event.country == 'Россия').all()
 
-rus_location = session.query(Event) \
-    .filter(Event.country == 'Россия').all()
+    for loc in rus_location:
+        event_location = loc.event
+        return event_location
 
-for loc in rus_location:
-    print(f'Забег {loc.event} проводится в России')
+def b_event(country):
+    border_location = session.query(Event) \
+        .filter(Event.country != 'Россия').all()
 
-
-border_location = session.query(Event) \
-    .filter(Event.country != 'Россия').all()
-
-for b_loc in border_location:
-    print(f'Забег {b_loc.event} проводится за границей')
-
-
-date_input = input()
-dates = session.query(Event) \
-    .filter(Event.date.like(f'%{date_input}')).all()
-
-for d_input in dates:
-    print(f'В указанном вами месяце {date_input} есть забег {d_input.event}')
+    for b_loc in border_location:
+        b_location = b_loc.event
+        return b_location
 
 
+def date_event(date):
+    dates = session.query(Event) \
+        .filter(Event.date.like(f'%{date}')).all()
+
+    for d_input in dates:
+        choosing_dates = d_input.date
+        return choosing_dates
+
+
+def event_date_country(dates, loc):
+    ew = session.query(Event) \
+        .filter(Event.date.like(f'%{dates}')). \
+        filter(Event.country == (loc)).all()
+    return ew
--- runbot.py
+++ runbot.py
@@ -1,10 +1,11 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
-from telegram import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, constants
 import logging
 
-# from queries import event_location
+from queries import event_date_country
 
 import os
+import re
 
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -21,15 +22,15 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 
 def main():
     API_KEY = os.environ.get("API_KEY")    
-    mybot = Updater(API_KEY, request_kwargs=PROXY)
+    mybot = Updater(API_KEY, request_kwargs=PROXY) 
     dp = mybot.dispatcher
 
     race_in = ConversationHandler(
         entry_points=[RegexHandler('^(RUN)$', go_to_menu, pass_user_data=True)],
         states={
-            "race_world": [RegexHandler('^(В России)$', race_in_russia, pass_user_data=True),
-                           RegexHandler('^(Что есть за границей?)$', race_in_border, pass_user_data=True)],
-            # "date_rus": [MessageHandler(Filters.text, available_date_rus, pass_user_data=True)]
+            "race_world": [MessageHandler(Filters.regex('^(Россия)$'), race_in_russia, pass_user_data=True),
+                           MessageHandler(Filters.regex('^(Заграница)$'), race_in_border, pass_user_data=True)],
+            "date_world": [MessageHandler(Filters.text, date_world, pass_user_data=True)],
         },
         fallbacks=[MessageHandler(
             Filters.text | Filters.video | Filters.photo | Filters.document, 
@@ -41,57 +42,72 @@ def main():
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(race_in)    
 
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling() #проверка, есть ли что-то новое
     mybot.idle()
 
 
-# def greet_user(bot, update):
-#     print('Вызван /start') 
-#     print(update)
-
-
 def greet_user(bot, update):
-    text = 'Вызван /start'
-    # print(text)
     my_keyboard = ReplyKeyboardMarkup([['RUN']])
-    update.message.reply_text(text, reply_markup=my_keyboard)
-
-def talk_to_me(bot, update):
-    user_text = update.message.text 
-    print(user_text)
-    update.message.reply_text(user_text)
+    print('Привет!')
+    update.message.reply_text('что-нибудь', reply_markup=my_keyboard, resize_keyboard=True)
 
 
 def go_to_menu(bot, update, user_data):
-    reply_keyboard = [["В России", "Что есть за границей?"]]
+    reply_keyboard = [["Россия"], ["Заграница"]]
     update.message.reply_text("Выбери где хочешь бежать", reply_markup=ReplyKeyboardMarkup(reply_keyboard), resize_keyboard=True)
     return "race_world"
 
 def race_in_russia(bot, update, user_data):
-    update.message.reply_text("Ты выбрал Россию. Укажи дату в формате «‎мм.гггг». Например: 04.2020")
-    user_choice = update.message.text
-    update.message.reply_text(user_choice, reply_markup=ReplyKeyboardRemove())
-    return "date_rus"
+    update.message.reply_text("Ты выбрал Россию. Укажи дату в формате «‎мм.гггг». Например: 04.2020", reply_markup=ReplyKeyboardRemove())
+    user_data['choosing_country'] = update.message.text
+    print(user_data['choosing_country'])
+    return "date_world"
 
 def race_in_border(bot, update, user_data):
-    update.message.reply_text("Ты выбрал заграницу. Укажи дату в формате «‎мм.гггг». Например: 04.2020")
-    user_choice1 = update.message.text
-    update.message.reply_text(user_choice1, reply_markup=ReplyKeyboardRemove())
-    return "date_b"
+    update.message.reply_text("Ты выбрал заграницу. Укажи дату в формате «‎мм.гггг». Например: 04.2020", reply_markup=ReplyKeyboardRemove())
+    user_data['choosing_country'] = update.message.text
+    print(user_data['choosing_country'])
+    return "date_world"
 
-def available_date_rus(bot, update, user_data):
+def date_world(bot, update, user_data):
     date = update.message.text
-    if len(date) != 7 :
+    match = str(re.fullmatch(r'\d{2}\.\d{4}', date))
+    country = 'За рубежом' if user_data['choosing_country'] == 'Заграница' else 'Россия'
+    print(country, date, "that's all")
+    if date not in match:
         update.message.reply_text("Ты ввёл неправильную дату")
-        return "date_rus"
-    # else:
-    #     update.message.reply_text("Вот что есть за этот месяц", event_location)
-    #     return ConversationHandler.END
+    else:
+        events_list = event_date_country(date, country)
+        events_names = " "
+        for i in events_list:
+            events_names = events_names + f'Забег "{i.event}" в {i.places} на дистанцию {i.distance} по типу {i.race_type} проводится {i.date}. Подробности: {i.links} \n\n'
+
+        if len(events_names) <= constants.MAX_MESSAGE_LENGTH:
+            return update.message.reply_text(f"Забеги в месяце {date}:\n\n{events_names[:-2]}")
+        
+        parts = []
+        while len(events_names) > 0:
+            if len(events_names) > constants.MAX_MESSAGE_LENGTH:
+                part = events_names[:constants.MAX_MESSAGE_LENGTH]
+                first = part.rfind('\n')
+                if first != -1:
+                    parts.append(part[:first])
+                    events_names = events_names[first:]
+                else:
+                    parts.append(part)
+                    events_names = events_names[constants.MAX_MESSAGE_LENGTH:]
+            else:
+                parts.append(events_names)
+                break
+        print(parts)
+        for part in parts:
+            update.message.reply_text(f"Забеги в месяце {date}:\n\n{part}")
+    return ConversationHandler.END
+    
 
 def dontknow(bot, update, user_data):
-    update.message.reply_text("Не понимаю")
-
+    update.message.reply_text("Не понимаю", reply_markup=greet_user(bot, update))
+    return ConversationHandler.END
 
 main()   
